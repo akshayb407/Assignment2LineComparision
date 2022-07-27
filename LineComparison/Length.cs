2674c2190eecce291d1dcbcaaa196809798f2af5
akshayb407 <akshayb407@gmail.com>
2022-07-27 23:49:24 +0530

    UC1_Calculate_Length

---
+++ LineComparison/Length.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Length
+    {
+        int x1 = 2, x2 = 3;
+
+        int y1 = 1, y2 = 2;
+
+        public void length()
+        {
+            int x = (x2-x1) * (x2-x1);  
+            int y = (y2-y1) * (y2-y1);
+            int z = x + y;
+
+            Console.WriteLine("Length of the Line is :" + Math.Sqrt(z));
+        }
+
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,6 +7,9 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program!");
+            Console.WriteLine("===========UC1===========");
+            Length UC1 = new Length();
+            UC1.length();
         }
     }
 }
